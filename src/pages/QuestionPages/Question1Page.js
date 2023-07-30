4922b321029c02e334e83422a2bc14dd1725752f
KiMJH <cale_kim@naver.com>
2023-07-30 22:45:49 +0900

    question-container items position

--- src/pages/QuestionPages/Question1Page.js
+++ src/pages/QuestionPages/Question1Page.js
@@ -1,9 +1,24 @@
+//css
+import classes from "./Question1Page.module.css";
+
 const Question1Page = () => {
   return (
-    <div>
-      <h1>Question Page 1</h1>
+    <div className={classes["question-container"]}>
+      <div className={classes["question-problem"]}>
+        <div className={classes["question-problem-number"]}>1</div>
+        <div className={classes["question-problem-text"]}>
+          안녕 캐치, 부동산이 너무 많아 우리 부동산이...
+        </div>
+      </div>
+
+      <div className={classes["question-answer"]}>
+        <div className={classes["question-image"]}></div>
+        <div className={classes["question-image"]}></div>
+        <div className={classes["question-button"]}></div>
+        <div className={classes["question-button"]}></div>
+      </div>
     </div>
   );
-}
+};
 
 export default Question1Page;
