9ef5d2f365c6e17bc3e9e6a2a0cb1c8063f5481d
KiMJH <cale_kim@naver.com>
2023-08-01 23:16:01 +0900

    Before meeting

---
+++ src/components/common/layout/AnswerContainer.js
@@ -0,0 +1,24 @@
+//css
+import classes from "./AnswerContainer.module.css";
+
+//imports
+import QuestionImage from "../QuestionImage";
+
+const AnswerContainer = ({ Problem, Image1, Image2 }) => {
+  return (
+    <>
+      <div className={classes["answer-text"]}>
+        <h1>해설</h1>
+      </div>
+
+      <div className={classes["answer"]}>
+        <div className={classes["answer-boder"]}></div>;
+        <QuestionImage Image1={Image1} Image2={Image2} />
+        {/* <AnswerText></AnswerText>
+        <AnswerButton /> */}
+      </div>
+    </>
+  );
+};
+
+export default AnswerContainer;
--- src/pages/GradePages/CorrectPage.js
+++ src/pages/GradePages/CorrectPage.js
@@ -1,12 +1,18 @@
 //css
 import classes from "./CorrectPage.module.css";
+import Image1 from "../../assets/images/start-button.png";
+
+//imports
+import AnswerContainer from "../../components/common/layout/AnswerContainer";
 
 const CorrectPage = () => {
   return (
-    <div>
-      <div className={["test"]}>
-        <h1>Hello</h1>
-      </div>
+    <div className={classes["question-page"]}>
+      <AnswerContainer
+        Problem={"안녕 캐치, 부동산이 너무 많아 우리 부동산이..."}
+        Image1={Image1}
+        Image2={Image1}
+      />
     </div>
   );
 };
