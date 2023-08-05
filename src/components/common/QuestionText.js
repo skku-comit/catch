560dd12a58bcd3385a972fb76bc235cbba799fd4
KiMJH <cale_kim@naver.com>
2023-08-06 02:31:51 +0900

    Text Popup

--- src/components/common/QuestionText.js
+++ src/components/common/QuestionText.js
@@ -1,22 +1,23 @@
+import React from "react";
 //css
 import classes from "./QuestionText.module.css";
 //imports
-import React, { useState, useEffect } from "react";
+import TypingText from "../dynamic/TypingText";
 
 const QuestionText = ({ Problem, checkDone }) => {
-  const lines = Problem.split("\n");
   return (
     <div
       className={`${classes["problem-text"]} ${
         checkDone !== "" && classes.done
       }`}
     >
-      {lines.map((line, index) => (
+      <TypingText text={Problem} />
+      {/* {lines.map((line, index) => (
         <React.Fragment key={index}>
           {line}
           <br />
         </React.Fragment>
-      ))}
+      ))} */}
     </div>
   );
 };
--- src/components/dynamic/TypingText.js
+++ src/components/dynamic/TypingText.js
@@ -1,8 +1,56 @@
+//css
+import classes from "./TypingText.module.css";
 //imports
 import React, { useState, useEffect } from "react";
 
-const TypingText = () => {
-  return;
+const TypingText = ({ text }) => {
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const count = setInterval(() => {
+      setDisplayText(displayText + text[currentIndex]);
+      setCurrentIndex(currentIndex + 1);
+    }, 100);
+    if (currentIndex === text.length) {
+      clearInterval(count);
+    }
+    return () => clearInterval(count);
+  });
+
+  return <div className={classes["text"]}>{displayText}</div>;
 };
 
 export default TypingText;
+
+//
+// const lines = text.split("\n");
+// const [currentLineIndex, setCurrentLineIndex] = useState(0);
+// const [currentCharIndex, setCurrentCharIndex] = useState(0);
+
+// useEffect(() => {
+//   const timer = setTimeout(() => {
+//     if (currentCharIndex < lines[currentLineIndex].length) {
+//       setCurrentCharIndex(currentCharIndex + 1);
+//     } else if (currentLineIndex < lines.length - 1) {
+//       setCurrentLineIndex(currentLineIndex + 1);
+//       setCurrentCharIndex(0);
+//     }
+//   }, 100); // 100ms마다 한 글자씩 출력
+
+//   return () => clearTimeout(timer);
+// }, [currentLineIndex, currentCharIndex]);
+
+// const displayedText = lines
+//   .slice(0, currentLineIndex + 1)
+//   .map((line, index) => {
+//     if (index < currentLineIndex) {
+//       return line;
+//     } else if (index === currentLineIndex) {
+//       return line.substring(0, currentCharIndex);
+//     }
+//     return "";
+//   })
+//   .join("\n");
+
+// return <div className={classes["text"]}>{displayedText}</div>;
