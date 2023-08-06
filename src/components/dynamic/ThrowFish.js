9502307a04a8579d79bb8f4fc2354c364c534ccb
KiMJH <cale_kim@naver.com>
2023-08-06 20:13:59 +0900

    Deleted AnswerPage, Container

--- src/components/dynamic/ThrowFish.js
+++ src/components/dynamic/ThrowFish.js
@@ -4,21 +4,14 @@ import classes from "./ThrowFish.module.css";
 //imports
 
 const ThrowFish = ({ checkWrong, onChangePage }) => {
-  //   const [animationFinished, setAnimationFinished] = useState(false);
-
   useEffect(() => {
     const timer = setTimeout(() => {
-      //   setAnimationFinished(true);
       onChangePage();
     }, 2000);
     return () => {
       clearTimeout(timer);
     };
   }, []);
-
-  //   useEffect(() => {
-  //     if (animationFinished) onChangePage(true);
-  //   }, [animationFinished]);
 
   return (
     <>
--- src/pages/Question1Page.js
+++ src/pages/Question1Page.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 //css
 import classes from "./Question1Page.module.css";
 
