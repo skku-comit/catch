dff103104ca77de831e09b22c991f26950a70486
KiMJH <cale_kim@naver.com>
2023-08-08 20:33:35 +0900

    Fadein when Popping up

--- src/components/common/Characters.js
+++ src/components/common/Characters.js
@@ -11,7 +11,7 @@ const Characters = ({ Number, CitizenImage }) => {
           backgroundImage: `url(${CitizenImage})`,
         }}
       >
-        {/* Number === ? 은 kid문제 정해지면 바꾸기 */}
+        {/* Number === ? 은 kid문제번호 정해지면 바꾸기 */}
         <div
           className={`${classes["kid"]} ${Number === 1 && classes.kidOn}`}
         ></div>
