81da6ce644865f343a1a2f1cad647105c3c4ad95
Taegon <taegon@cusee.net>
2023-08-11 01:18:03 +0900

    Update QuestionButton.js

--- src/components/common/QuestionButton.js
+++ src/components/common/QuestionButton.js
@@ -1,35 +1,38 @@
 //imports
-import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 //css
-import classes from "./QuestionButton.module.css";
+import classes from './QuestionButton.module.css';
 
-const QuestionButton = ({ answer, checkDone, onCheckAnswer }) => {
-  const [userAnswer, setUserAnswer] = useState("initial");
-  useEffect(() => {
-    if (userAnswer !== "initial") onCheckAnswer(userAnswer);
-  }, [userAnswer]);
+const QuestionButton = ({ onCheckAnswer }) => {
+  // const correctLink = "./ans";
+  // const wrongLink = "./wrg";
+
+  const [userAnswer, setUserAnswer] = useState('intial');
 
   const AclickHandler = () => {
-    setUserAnswer("A");
+    setUserAnswer('A');
+    onCheckAnswer(userAnswer);
+    alert('Clicked A');
   };
   const BclickHandler = () => {
-    setUserAnswer("B");
+    setUserAnswer('B');
+    onCheckAnswer(userAnswer);
+    alert('Clicked B');
   };
 
   return (
-    <div className={classes["button-container"]}>
-      <button
-        className={`${classes.button} ${classes["defaultA-button"]} ${
-          answer === "B" && checkDone !== "" && classes.done
-        }`}
-        onClick={AclickHandler}
-      />
-      <button
-        className={`${classes.button} ${classes["defaultB-button"]} ${
-          answer === "A" && checkDone !== "" && classes.done
-        }`}
-        onClick={BclickHandler}
-      />
+    // 해설화면 확인하기 : 나중에 Timer로 구현예정
+    <div className={classes['button-container']}>
+      <button className={classes['correct-link']} onClick={AclickHandler} />
+      <button className={classes['wrong-link']} onClick={BclickHandler} />
+
+      {/* <Link to={correctLink} className={classes["correct-link"]}>
+        <button className={classes['correct-button']}/>
+      </Link>
+      <Link to={correctLink} className={classes["wrong-link"]}>
+        <button className={classes['wrong-button']}/>
+      </Link> */}
     </div>
   );
 };
