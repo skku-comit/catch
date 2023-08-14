1d373225a5183f7f3c8632c32ddd182d063a7ba8
KiMJH <cale_kim@naver.com>
2023-08-14 16:42:17 +0900

    StarPage flash & click button Ani

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,7 +1,7 @@
+import { motion, useAnimation } from "framer-motion";
 // hooks
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { motion, useAnimation } from "framer-motion";
 
 // css
 import classes from "./StartPage.module.css";
