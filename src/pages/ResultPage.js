77ca5a2e23de8738f0f6b5ea88ac9368abcbd6a9
Ji__ho <cale.jiho@gmail.com>
2023-08-18 00:48:30 +0900

    Score

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,6 +1,6 @@
 import { delay, motion } from "framer-motion";
 //
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 //imports
 import Level from "../data/level";
 import { Link } from "react-router-dom";
@@ -11,6 +11,23 @@ const ResultPage = () => {
   const curExpContext = useContext(Level);
   const curExp = parseInt(curExpContext.exp);
   const level = Math.floor(curExp / 4) + 1;
+
+  const [score, setScore] = useState(0);
+  console.log("맞춘개수 : ");
+  console.log(curExp - level + 1);
+
+  useEffect(() => {}, [score]);
+
+  const onAnimationComplete = () => {
+    console.log(score);
+    console.log("Occured!");
+    const interval = setTimeout(() => {
+      setScore((prevScore) => prevScore + 1);
+    }, 500);
+    return () => {
+      clearTimeout(interval);
+    };
+  };
 
   const resetExp = () => {
     curExpContext.setExp(0);
@@ -57,6 +74,7 @@ const ResultPage = () => {
         return "/images/etc/particle_5.png";
     }
   };
+
   return (
     <div
       className={classes["background"]}
@@ -68,31 +86,46 @@ const ResultPage = () => {
         className={classes["expResult"]}
         src={selectResultImage(level)}
         alt="resultExpImage"
-        initial={{
-          y: -50,
-          opacity: 0,
-        }}
-        animate={{
-          y: 0,
-          opacity: 1,
-        }}
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{
           delay: 0.75,
           duration: 0.5,
           ease: [0, 0, 0, 1],
         }}
       />
+      <motion.div
+        className={classes["score-container"]}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 0.2,
+          delay: 1.5,
+        }}
+      >
+        {score < curExp - level + 1 ? (
+          <motion.div
+            className={classes["score"]}
+            animate={{ opacity: [1, 0, 0, 1], y: [0, -80, 0.1, 0] }}
+            transition={{ duration: 3, times: [0, 0.98, 0.99, 1] }}
+            onAnimationComplete={onAnimationComplete()}
+          >
+            {score}
+          </motion.div>
+        ) : (
+          <div className={classes["score"]}>{score}</div>
+        )}
+
+        <div className={classes["score"]}>/</div>
+        <div className={classes["score"]}>12</div>
+      </motion.div>
       <Link to={"/"}>
         <button className={classes["button"]} onClick={resetExp} />
       </Link>
       <motion.div
         className={classes["particle"]}
-        style={{
-          backgroundImage: `url(${selectParticleImage(level)})`,
-        }}
-        initial={{
-          rotateZ: 0,
-        }}
+        style={{ backgroundImage: `url(${selectParticleImage(level)})` }}
+        initial={{ rotateZ: 0 }}
         animate={{
           scale: [1, 1.5, 1],
           rotateZ: 360,
